afe2e6b23795505678db5311312f24312ca46cf8
MattReamer <58491528+MattReamer@users.noreply.github.com>
2020-02-26 13:55:12 -0500

    Update index.js

--- src/components/Widget/components/Launcher/index.js
+++ src/components/Widget/components/Launcher/index.js
@@ -8,13 +8,13 @@ import Badge from './components/Badge';
 import './style.scss';
 
 const Launcher = ({ toggle, chatOpened, badge }) =>
-  <button type="button" className={chatOpened ? 'rcw-launcher rcw-hide-sm' : 'rcw-launcher'} onClick={toggle}>
-    <Badge badge={badge} />
-    {chatOpened ?
-      <img src={close} className="rcw-close-launcher" alt="" /> :
-      <img src={openLauncher} className="rcw-open-launcher" alt="" />
-    }
-  </button>;
+//   <button type="button" className={chatOpened ? 'rcw-launcher rcw-hide-sm' : 'rcw-launcher'} onClick={toggle}>
+//     <Badge badge={badge} />
+//     {chatOpened ?
+//       <img src={close} className="rcw-close-launcher" alt="" /> :
+//       <img src={openLauncher} className="rcw-open-launcher" alt="" />
+//     }
+//   </button>;
 
 Launcher.propTypes = {
   toggle: PropTypes.func,
